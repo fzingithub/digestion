5689d3374348c996370a07960892a3af2e7873cc
chier <chier@chiers-MacBook-Pro.local>
2018-11-16 20:03:31 +0800

    添加图片爬虫单元测试

--- food_eliminate/climb_data/tests.py
+++ food_eliminate/climb_data/tests.py
@@ -3,12 +3,21 @@ from django.test import TestCase
 # Create your tests here.
 from .climb import Mint
 
+
 class ClimbTests(TestCase):
+    url = "http://www.boohee.com/food/group/1"
+
     def testHtmlBody(self):
-        url = "http://www.boohee.com/food/group/1"
-        Mint().getHtmlBody(url)
+        Mint().getHtmlBody(self.url)
+
     def testClimbData(self):
-        url = "http://www.boohee.com/food/group/1"
-        Mint().climbData("", url)
+        Mint().climbData("", self.url)
+
     def testCreateUrls(self):
-        Mint().allFoodData()
+        Mint().climbFoodList(self.url)
+
+    def testClimbPics(self):
+        Mint().climbPics(self.url)
+
+    def testMyMain(self):
+        Mint().myMain()
