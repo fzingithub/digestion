0f8a8a8227b5730b1ed23704935fdbdd3436f539
chier <chier@chiers-MacBook-Pro.local>
2018-11-16 20:03:17 +0800

    添加图片爬虫

--- food_eliminate/climb_data/climb.py
+++ food_eliminate/climb_data/climb.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from urllib import request
 from common import helper
+import urllib
+import configparser
 import logging
 import re
 import ssl
 
-
+conf = configparser.ConfigParser()
+conf.read("config/mysql.conf")
 logger = logging.getLogger(__name__)
 
 
@@ -13,10 +15,10 @@ class Mint(object):
     def __init__(self):
         self.pagePattern = '.*?<span>共([0-9]+)页.*?</span>'
         self.listPattern = '.*?<li>.*?<a href="(.*?)".*?<img src="(.*?)".*?<h3>(.*?)</h3>.*?</li>'
+        ssl._create_default_https_context = ssl._create_unverified_context
 
     def getHtmlBody(self, url):
-        ssl._create_default_https_context = ssl._create_unverified_context
-        urlResponse = request.urlopen(url)
+        urlResponse = urllib.request.urlopen(url)
         urlBody = urlResponse.read().decode('utf-8')
         return urlBody
 
@@ -27,20 +29,48 @@ class Mint(object):
         return textList
 
     def insertDB(self, allData):
-        sql = "INSERT INTO food_list(food_name, detail_url, pic_url) VALUES (%s, %s, %s)"
+        sql = "INSERT INTO food_list(food_name, detail_url, img_url, food_type) VALUES (%s, %s, %s, %s)"
         helper.DB().insert_many(sql, allData)
 
-    def allFoodData(self):
-        url = "https://food.hiyd.com/"
-
+    def climbFoodList(self, url):
         for i in range(1, 11):
             groupUrl = "%slist-%d-html" % (url, i)
-            pages = self.climbData(self.pagePattern, groupUrl)
-            for page in range(1, int(pages[0]) + 1):
+            pages = int(self.climbData(self.pagePattern, groupUrl)[0])
+
+            allData = list()
+
+            for page in range(1, pages + 1):
                 pageUrl = "%s?page=%d" % (groupUrl, page)
                 foodList = self.climbData(self.listPattern, pageUrl)
-
-                allData = list()
                 for it in foodList:
-                    allData.append((it[2], "https:" + it[0], it[1]))
-                self.insertDB(allData)
+                    allData.append((it[2], "https:" + it[0], it[1], i))
+
+            self.insertDB(allData)
+
+    def climbPics(self, foodList):
+        imgPath = conf.get("staticPath", "img_path")
+        imgData = list()
+        for it in foodList:
+            imgUrl = it['img_url']
+            foodId = it['food_id']
+            foodType = it['food_type']
+
+            localImgUrl = "food_type_{}/image_{}.jpg".format(foodType, foodId)
+
+            logger.info(it)
+            logger.info(localImgUrl)
+
+            try: 
+                urllib.request.urlretrieve(
+                    imgUrl, "{}{}".format(imgPath, localImgUrl))
+                imgData.append((foodId, localImgUrl))
+            except Exception as e:
+                imgData.append((foodId, ''))
+                logger.error(e)
+        return imgData
+
+    def myMain(self):
+        foodList = helper.DB().fetch_all("SELECT * FROM food_list")
+        imgData = self.climbPics(foodList)
+        helper.DB().insert_many(
+            "INSERT INTO image_list(food_id, img_url) VALUES (%s, %s)", imgData)
